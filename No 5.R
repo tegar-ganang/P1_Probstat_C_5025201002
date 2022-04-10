5719511e1fb5055fe87780463a039892165d0530
tegar-ganang <tegar.205025@mhs.its.ac.id>
2022-04-10 14:08:10 +0700

    Update No 5.R

--- No 5.R
+++ No 5.R
@@ -1,7 +1,29 @@
-set.seed(1)
-library(ggplot2)
-theme_set(theme_bw())
 lambda <- 3
+#5a
+DistExponensial <- function(lambda, x){
+  print((1/lambda),exp(x/lambda*-1))
+}
 
-data <- replicate(1000, mean(rexp(10,lambda)))
-data
+#5b
+set.seed(1)
+N <- 10
+y_rexp <- rexp(N, lambda)
+hist(y_rexp,breaks = 5, 
+     main = "10 data")
+
+N <- 100
+y_rexp <- rexp(N, lambda)
+hist(y_rexp,breaks = 50, main = "100 data")
+
+N <- 1000
+y_rexp <- rexp(N, lambda)
+hist(y_rexp, breaks = 500, main = "1000 data")
+
+N <- 10000
+y_rexp <- rexp(N, lambda)
+hist(y_rexp, breaks = 5000, main = "10000 data")
+
+#5c
+mean(rexp(100, lambda))
+
+var(rexp(100, lambda))
